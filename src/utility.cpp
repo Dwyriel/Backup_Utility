4b0246ced53f0beb7eee09fcf85b4598d07ed9d3
Dwyriel <dwyriel333@hotmail.com>
2022-11-01 13:33:46 -0300

    MessageBox

---
+++ src/utility.cpp
@@ -0,0 +1,22 @@
+#include "utility.h"
+
+#define PIXMAP_SIZE 60
+#define MSGBOX_STYLE_SHEET "font-size: 16px;"
+
+void Utility::showError(QWidget *parent, QString title, QString body){
+    auto msgBox = QMessageBox(QMessageBox::Icon::Critical, title, body, QMessageBox::Ok, parent);
+    QPixmap pixmap(":/resources/icon/dialog-error.ico");
+    pixmap = pixmap.scaled(PIXMAP_SIZE, PIXMAP_SIZE, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    msgBox.setIconPixmap(pixmap);
+    msgBox.setStyleSheet(MSGBOX_STYLE_SHEET);
+    msgBox.exec();
+}
+
+void Utility::showWarning(QWidget *parent, QString title, QString body){
+    auto msgBox = QMessageBox(QMessageBox::Icon::Warning, title, body, QMessageBox::Ok, parent);
+    QPixmap pixmap(":/resources/icon/dialog-warning.ico");
+    pixmap = pixmap.scaled(PIXMAP_SIZE, PIXMAP_SIZE, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    msgBox.setIconPixmap(pixmap);
+    msgBox.setStyleSheet(MSGBOX_STYLE_SHEET);
+    msgBox.exec();
+}
---
+++ src/utility.h
@@ -0,0 +1,12 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <QMessageBox>
+
+namespace Utility
+{
+void showError( QWidget *parent, QString title, QString body);
+void showWarning( QWidget *parent, QString title, QString body);
+};
+
+#endif // UTILITY_H
